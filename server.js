445c02756f8dc1bf60fad342d158aafd3870bdeb
Alok <alokthakur609@gmail.com>
2025-05-14 16:43:10 +0530

    Database connection

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const app = require('./app');
+const connectDB = require('./config/db');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
-
+connectDB();
 
 // Start the server
 app.listen(PORT, () => {
